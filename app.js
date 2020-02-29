52b7a1124cfdb0bee8d148e5d2f40ce069fa1ca0
Marcos <mp2772@gmail.com>
2020-02-29 16:10:29 -0500

    modified .env

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-    .connect("mongodb://localhost/messageboardexample", {
+    .connect(process.env.MONGODB_URI, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
